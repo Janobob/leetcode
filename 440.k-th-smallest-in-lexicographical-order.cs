90eaeb5670f84c0e6d2123d18f4d736aa08f4a88
Jan Werren <jan.werren@outlook.com>
2024-09-22 10:07:48 +0200

    feat([440]): submission

--- 440.k-th-smallest-in-lexicographical-order.cs
+++ 440.k-th-smallest-in-lexicographical-order.cs
@@ -7,8 +7,38 @@
 // @lc code=start
 public class Solution {
     public int FindKthNumber(int n, int k) {
-        var numbers = Enumerable.Range(1, n).Select(x => x.ToString()).OrderBy(x => x).ToList();
-        return numbers.Select(x => int.Parse(x)).ElementAt(k - 1);
+        // start from 1, k = 0
+        int curr = 1;
+        k--; // decrement k to make it 0-based
+        
+        // as long as k is greater than 0, keep searching
+        while (k > 0) {
+            long steps = CalculateSteps(n, curr, curr + 1);
+            // if steps <= k, k is in the subtree of curr
+            if (steps <= k) {
+                // move to next neighbor node
+                curr++;
+                k -= (int)steps;
+                continue;
+            }
+            // if steps > k, k is in the subtree of curr * 10
+            curr *= 10;
+            k--;
+        }
+        
+        // return the current node
+        return curr;
+    }
+
+    // calculate steps between n1 and n2
+    private long CalculateSteps(int n, long n1, long n2) {
+        long steps = 0;
+        while (n1 <= n) {
+            steps += Math.Min(n + 1, n2) - n1;
+            n1 *= 10;
+            n2 *= 10;
+        }
+        return steps;
     }
 }
 // @lc code=end
