64fe0786af36188c9e4eb6e35ac164fce75b41a0
Jan Werren <jan.werren@outlook.com>
2024-10-01 16:53:23 +0200

    feat([1497]): submission

--- 1497.check-if-array-pairs-are-divisible-by-k.cs
+++ 1497.check-if-array-pairs-are-divisible-by-k.cs
@@ -7,28 +7,42 @@
 // @lc code=start
 public class Solution {
     public bool CanArrange(int[] arr, int k) {
-        var initialLength = arr.Length;
-        var pairs = new Dictionary<int, int>();
-        
+        var remainders = new Dictionary<int, int>();
 
         foreach (var num in arr) {
-            foreach(var remainder in arr.Reverse().Where(x => x != num)){
-                if(num + remainder != 0 && (num + remainder) % k == 0){
-                    pairs.Add(num, remainder);
-                    Console.WriteLine(num + " " + remainder);
-                    arr = arr.Where(x => x != num).ToArray();
-                    arr = arr.Where(x => x != pairs[num]).ToArray();
-                    break;
-                }
+            var remainder = num % k;
+
+            // keep the remainder positive
+            remainder = remainder < 0 ? remainder + k : remainder;
+
+            if(remainders.ContainsKey(remainder)) {
+                remainders[remainder]++;
+            } else {
+                remainders[remainder] = 1;
             }
         }
 
-        Console.WriteLine(pairs.Count);
+        // check if the number of elements with remainder i is equal to the number of elements with remainder k - i
+        for (int i = 1; i < k; i++) {
+            if (!remainders.ContainsKey(i)) {
+                remainders[i] = 0;
+            }
 
-        var left = pairs.Select(x => x.Key).ToList();
-        var right = pairs.Select(x => x.Value).ToList();
+            if (!remainders.ContainsKey(k - i)) {
+                remainders[k - i] = 0;
+            }
 
-        return left.Sum() % k == 0 && right.Sum() % k == 0;
+            if (remainders[i] != remainders[k - i]) {
+                return false;
+            }
+        }
+
+        // check if the number of elements with remainder 0 is even
+        if (remainders.ContainsKey(0) && remainders[0] % 2 != 0) {
+            return false;
+        }
+
+        return true;
     }
 }
 // @lc code=end
