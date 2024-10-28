0bd261ed7a97d59ba4c620e67f9bb188a8533ebf
Jan Werren <jan.werren@outlook.com>
2024-10-28 12:27:12 +0100

    feat([1]): submission with hashmap/dictionary
    63/63 cases passed (1 ms)
    Your runtime beats 98.88 % of csharp submissions
    Your memory usage beats 12.58 % of csharp submissions (48.3 MB)

--- 1.two-sum.cs
+++ 1.two-sum.cs
@@ -7,15 +7,21 @@
 // @lc code=start
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.Length; i++){
-            for(int j = 0; j < nums.Length; j++){
-                if(i != j && nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+        var dict = new Dictionary<int, int>();
+
+        for(int i = 0; i < nums.Length; i++) {
+            // calculate the complement
+            int complement = target - nums[i];
+
+            // when the complement is in the dictionary, look up the index of the complement
+            if(dict.ContainsKey(complement)) {
+                // return the index of the complement and the current index
+                return new int[] {dict[complement], i};
             }
+            dict[nums[i]] = i;
         }
-        
-        return new int[0];
+
+        return new int[] {};
     }
 }
 // @lc code=end
